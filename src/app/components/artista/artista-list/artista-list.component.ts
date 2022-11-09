9c0fe1a1a903f2b3b681f1ea08d2ab5cbadceef6
Reinaldo <naldos15@hotmail.com>
2022-11-09 11:18:45 -0300

    listagem de artistas

--- src/app/components/artista/artista-list/artista-list.component.ts
+++ src/app/components/artista/artista-list/artista-list.component.ts
@@ -10,13 +10,27 @@ import { ArtistaService } from 'src/app/services/artista.service';
 export class ArtistaListComponent implements OnInit {
 
   products: Artista[] = [];
-
+  cols: any[] = [];
   
 
     constructor(private artistaService: ArtistaService) { }
 
     ngOnInit() {
         this.artistaService.getProductsSmall().then(data => this.products = data);
+        this.cols = [
+          { field: 'code', header: 'Nome', width: '20%' },
+          {field: 'name', header: 'Gênero Musical', width: '20%' },
+          { field: 'category', header: 'País de Origem', width: '20%' },      
+          { field: 'quantity', header: 'Integrantes', width: '20%' },      
+      ];
+    }
+
+    editProduct(artista: Artista[]): void {
+      console.log("")
+    }
+
+    deleteProduct(artista: Artista[]): void {
+      console.log("")
     }
 
 }
